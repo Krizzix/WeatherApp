33522e972cdd991f308af40a85fa88ea273323c4
Krizzix <chi1291999@gmail.com>
2024-01-08 13:56:52 +0700

    Fix react hooks  missing depedencies and add alt to img

--- src/components/GetCurrentWeather.jsx
+++ src/components/GetCurrentWeather.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useContext, useEffect, useState } from "react";
-import WeatherContext from "./context/WeatherContext";
-import classes from "./css/GetCurrentWeather.module.css";
+import React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import WeatherContext from './context/WeatherContext';
+import classes from './css/GetCurrentWeather.module.css';
 
 // import Thunderstorm from "./weather/Thunderstorm.svg";
 export default function GetCurrentWeather() {
@@ -27,17 +27,17 @@ export default function GetCurrentWeather() {
     // },${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
     // setDate(dateConvert);
     //
-  }, [lon]);
+  }, [lon]); // eslint-disable-line react-hooks/exhaustive-deps
   useEffect(() => {
     const d = new Date(new Date().getTime() - timezone * 1000);
     const days = [
-      "Chủ nhật",
-      "Thứ hai",
-      "Thứ ba",
-      "Thứ tư",
-      "Thứ năm",
-      "Thứ sáu",
-      "Thứ bảy",
+      'Chủ nhật',
+      'Thứ hai',
+      'Thứ ba',
+      'Thứ tư',
+      'Thứ năm',
+      'Thứ sáu',
+      'Thứ bảy',
     ];
     setDay(days[d.getDay()]);
     const dateConvert = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
@@ -47,26 +47,27 @@ export default function GetCurrentWeather() {
     return <p>Không tìm thấy tên thành phố</p>;
   } else {
     return (
-      <div className={classes["weather-container"]}>
+      <div className={classes['weather-container']}>
         {Object.keys(weatherObj).length !== 0 && (
           <>
             <div>
               <h3 className={classes.day}>{day}</h3>
               <h3 className={classes.date}>{date}</h3>
-              <div className={classes["img-box"]}>
+              <div className={classes['img-box']}>
                 <img
                   src={`${process.env.PUBLIC_URL}/assets/${weatherObj.weather[0].main}.svg`}
-                  className={classes["img-weather"]}
+                  className={classes['img-weather']}
+                  alt='img-weather'
                 />
               </div>
             </div>
-            <div className={classes["weather-detail"]}>
+            <div className={classes['weather-detail']}>
               <div className={classes.temperature}>
                 {(weatherObj.main.temp - 273).toFixed(0)} &#8451;
               </div>
-              <div className={classes["line-hor"]}></div>
-              <div className={classes["weather-des"]}>
-                <div className={classes["description"]}>
+              <div className={classes['line-hor']}></div>
+              <div className={classes['weather-des']}>
+                <div className={classes['description']}>
                   {weatherObj.weather[0].description}
                 </div>
                 <div>{`${name},${sys.country}`}</div>
